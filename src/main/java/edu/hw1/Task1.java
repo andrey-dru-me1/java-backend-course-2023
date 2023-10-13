587fab98652a127d3d7ca39bdd4de5ccb2ffb774
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-13 23:50:27 +0700

    Rewrite 1st task for more reusability

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -6,23 +6,23 @@ public class Task1 {
 
     public static int minutesToSeconds(@NotNull String string) {
         byte[] bytes = string.getBytes();
-        int i = 0, seconds = 0, d;
+        int i = 0, seconds = 0;
 
         if(bytes.length != 5) return -1;
 
-        if((d = Character.getNumericValue((char)bytes[i++])) < 0 || d > 2) return -1;
-        seconds += d * 60 * 10;
+        final int[] multipliers = {60*10, 60, 10, 1};
+        int iMultiplier = 0;
 
-        if((d = Character.getNumericValue((char)bytes[i++])) < 0 || d > 4) return -1;
-        seconds += d * 60;
-
-        if((char)bytes[i++] != ':') return -1;
-
-        if((d = Character.getNumericValue((char)bytes[i++])) <0 || d >= 6) return -1;
-        seconds += d * 10;
-
-        if((d = Character.getNumericValue((char)bytes[i])) < 0) return -1;
-        seconds += d;
+        for(char p : "23:59".toCharArray()) {
+            int pDigit = Character.getNumericValue(p);
+            if(pDigit < 0) {
+                if(p != (char)bytes[i++]) return -1;
+                else continue;
+            }
+            int digit = Character.getNumericValue(bytes[i++]);
+            if(digit > pDigit) return -1;
+            seconds += multipliers[iMultiplier++] * digit;
+        }
 
         return seconds;
     }
