e9bab9f79d2b9f9f4796eea70dfd71c84ae6ad36
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-13 23:52:31 +0700

    Reformat code

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -8,19 +8,26 @@ public class Task1 {
         byte[] bytes = string.getBytes();
         int i = 0, seconds = 0;
 
-        if(bytes.length != 5) return -1;
+        if (bytes.length != 5) {
+            return -1;
+        }
 
-        final int[] multipliers = {60*10, 60, 10, 1};
+        final int[] multipliers = {60 * 10, 60, 10, 1};
         int iMultiplier = 0;
 
-        for(char p : "23:59".toCharArray()) {
+        for (char p : "23:59".toCharArray()) {
             int pDigit = Character.getNumericValue(p);
-            if(pDigit < 0) {
-                if(p != (char)bytes[i++]) return -1;
-                else continue;
+            if (pDigit < 0) {
+                if (p != (char) bytes[i++]) {
+                    return -1;
+                } else {
+                    continue;
+                }
             }
             int digit = Character.getNumericValue(bytes[i++]);
-            if(digit > pDigit) return -1;
+            if (digit > pDigit) {
+                return -1;
+            }
             seconds += multipliers[iMultiplier++] * digit;
         }
 
