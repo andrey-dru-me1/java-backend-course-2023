b8a0fc048f7132fb400cb6e4ad2c1fdde62ea9f7
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-15 18:08:31 +0700

    Implement 3rd task

---
+++ src/test/java/edu/hw1/Task3Test.java
@@ -0,0 +1,16 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class Task3Test {
+
+    @Test
+    void isNestable() {
+        assertThat(Task3.isNestable(new int[] {1, 2, 3, 4}, new int[] {0, 6})).isEqualTo(true);
+        assertThat(Task3.isNestable(new int[] {3, 1}, new int[] {4, 0})).isEqualTo(true);
+        assertThat(Task3.isNestable(new int[] {9, 9, 8}, new int[] {8, 9})).isEqualTo(false);
+        assertThat(Task3.isNestable(new int[] {1, 2, 3, 4}, new int[] {2, 3})).isEqualTo(false);
+    }
+}
