2df33c82d872a298b77e4ae9ef0db47f3cd91e2f
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-15 20:37:30 +0700

    Add test names

--- src/test/java/edu/hw1/Task3Test.java
+++ src/test/java/edu/hw1/Task3Test.java
@@ -1,5 +1,6 @@
 package edu.hw1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class Task3Test {
 
     @Test
+    @DisplayName("Проверка на вложенность массива")
     void isNestable() {
         assertThat(Task3.isNestable(new int[] {1, 2, 3, 4}, new int[] {0, 6})).isEqualTo(true);
         assertThat(Task3.isNestable(new int[] {3, 1}, new int[] {4, 0})).isEqualTo(true);
--- src/test/java/edu/hw1/Task4Test.java
+++ src/test/java/edu/hw1/Task4Test.java
@@ -1,5 +1,6 @@
 package edu.hw1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class Task4Test {
 
     @Test
+    @DisplayName("Исправление строки с попарно перепутанными символами")
     void fixString() {
         assertThat(Task4.fixString("123456")).isEqualTo("214365");
         assertThat(Task4.fixString("hTsii  s aimex dpus rtni.g")).isEqualTo("This is a mixed up string.");
