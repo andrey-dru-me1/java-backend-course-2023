068baa95786797caa23a4fd7d6e9536016568888
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-17 14:03:11 +0700

    Modify code with accordance to stylechecker

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,7 +1,7 @@
 package edu.hw1;
 
-import org.jetbrains.annotations.NotNull;
 import java.util.Scanner;
+import org.jetbrains.annotations.NotNull;
 
 public final class Task1 {
 
@@ -10,14 +10,19 @@ public final class Task1 {
 
     @SuppressWarnings("magicnumber")
     public static int minutesToSeconds(@NotNull String string) {
-        try(Scanner scanner = new Scanner(string).useDelimiter(":")) {
-            if(!scanner.hasNextInt()) return -1;
+        try (Scanner scanner = new Scanner(string).useDelimiter(":")) {
+            if (!scanner.hasNextInt()) {
+                return -1;
+            }
             int minutes = scanner.nextInt();
-            if (minutes < 0) return -1;
 
-            if(!scanner.hasNextInt()) return -1;
+            if (!scanner.hasNextInt()) {
+                return -1;
+            }
             int seconds = scanner.nextInt();
-            if (seconds < 0 || seconds >= 60) return -1;
+            if (minutes < 0 || seconds < 0 || seconds >= 60) {
+                return -1;
+            }
 
             seconds += minutes * 60;
             return seconds;
