c81c0fb5b4d8e56cb4e235053f2204420ae4bff2
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-17 13:30:01 +0700

    Remove extra annotation

--- src/main/java/edu/hw1/Task4.java
+++ src/main/java/edu/hw1/Task4.java
@@ -1,13 +1,12 @@
 package edu.hw1;
 
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public final class Task4 {
     private Task4() {
     }
 
-    @Contract("_ -> new") public static @NotNull String fixString(@NotNull String str) {
+    public static @NotNull String fixString(@NotNull String str) {
         char[] chars = str.toCharArray();
         for (int i = 0; i < str.length() - 1; i += 2) {
             char c = chars[i];
