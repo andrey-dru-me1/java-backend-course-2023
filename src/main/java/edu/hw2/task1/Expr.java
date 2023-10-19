7217a8554766bbe797912645d2072529c691243c
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-19 20:38:23 +0700

    Separate expressions onto different files and enable google java codestyle

--- src/main/java/edu/hw2/task1/Expr.java
+++ src/main/java/edu/hw2/task1/Expr.java
@@ -1,35 +1,5 @@
 package edu.hw2.task1;
 
-public sealed interface Expr {
-    double evaluate();
-
-    record Constant(double number) implements Expr {
-        @Override public double evaluate() {
-            return number;
-        }
-    }
-
-    record Negate(Expr expr) implements Expr {
-        @Override public double evaluate() {
-            return -expr.evaluate();
-        }
-    }
-
-    record Exponent(Expr expr, int degree) implements Expr {
-        @Override public double evaluate() {
-            return Math.pow(expr.evaluate(), degree);
-        }
-    }
-
-    record Addition(Expr a, Expr b) implements Expr {
-        @Override public double evaluate() {
-            return a.evaluate() + b.evaluate();
-        }
-    }
-
-    record Multiplication(Expr a, Expr b) implements Expr {
-        @Override public double evaluate() {
-            return a.evaluate() * b.evaluate();
-        }
-    }
+public sealed interface Expr permits Addition, Constant, Exponent, Multiplication, Negate {
+  double evaluate();
 }
--- src/test/java/edu/hw2/Task1Test.java
+++ src/test/java/edu/hw2/Task1Test.java
@@ -1,32 +1,38 @@
 package edu.hw2;
 
-import edu.hw2.task1.Expr;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import edu.hw2.task1.Addition;
+import edu.hw2.task1.Constant;
+import edu.hw2.task1.Exponent;
+import edu.hw2.task1.Multiplication;
+import edu.hw2.task1.Negate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class Task1Test {
-    @Test @DisplayName("Перевод минут в секунды")
-    void test() {
-        var two = new Expr.Constant(2);
-        assertThat(two.evaluate()).isEqualTo(2.);
+  @Test
+  @DisplayName("Перевод минут в секунды")
+  void test() {
+    var two = new Constant(2);
+    assertThat(two.evaluate()).isEqualTo(2.);
 
-        var four = new Expr.Constant(4);
-        assertThat(four.evaluate()).isEqualTo(4.);
+    var four = new Constant(4);
+    assertThat(four.evaluate()).isEqualTo(4.);
 
-        var negOne = new Expr.Negate(new Expr.Constant(1));
-        assertThat(negOne.evaluate()).isEqualTo(-1.);
+    var negOne = new Negate(new Constant(1));
+    assertThat(negOne.evaluate()).isEqualTo(-1.);
 
-        var sumTwoFour = new Expr.Addition(two, four);
-        assertThat(sumTwoFour.evaluate()).isEqualTo(6.);
+    var sumTwoFour = new Addition(two, four);
+    assertThat(sumTwoFour.evaluate()).isEqualTo(6.);
 
-        var mult = new Expr.Multiplication(sumTwoFour, negOne);
-        assertThat(mult.evaluate()).isEqualTo(-6.);
+    var mult = new Multiplication(sumTwoFour, negOne);
+    assertThat(mult.evaluate()).isEqualTo(-6.);
 
-        var exp = new Expr.Exponent(mult, 2);
-        assertThat(exp.evaluate()).isEqualTo(36.);
+    var exp = new Exponent(mult, 2);
+    assertThat(exp.evaluate()).isEqualTo(36.);
 
-        var res = new Expr.Addition(exp, new Expr.Constant(1));
-        assertThat(res.evaluate()).isEqualTo(37.);
-    }
+    var res = new Addition(exp, new Constant(1));
+    assertThat(res.evaluate()).isEqualTo(37.);
+  }
 }
