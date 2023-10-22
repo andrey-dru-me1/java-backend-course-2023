22ca00198bce01ebd4d7bcc1c3c745654df04656
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-23 02:25:29 +0700

    Add negative test for 3rd task

--- src/test/java/edu/hw2/Task1Test.java
+++ src/test/java/edu/hw2/Task1Test.java
@@ -1,38 +1,34 @@
 package edu.hw2;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import edu.hw2.task1.Addition;
-import edu.hw2.task1.Constant;
-import edu.hw2.task1.Exponent;
-import edu.hw2.task1.Multiplication;
-import edu.hw2.task1.Negate;
+import edu.hw2.task1.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class Task1Test {
-  @Test
-  @DisplayName("Математические выражения")
-  void test() {
-    var two = new Constant(2);
-    assertThat(two.evaluate()).isEqualTo(2.);
+    @Test
+    @DisplayName("Математические выражения")
+    void test() {
+        var two = new Constant(2);
+        assertThat(two.evaluate()).isEqualTo(2.);
 
-    var four = new Constant(4);
-    assertThat(four.evaluate()).isEqualTo(4.);
+        var four = new Constant(4);
+        assertThat(four.evaluate()).isEqualTo(4.);
 
-    var negOne = new Negate(new Constant(1));
-    assertThat(negOne.evaluate()).isEqualTo(-1.);
+        var negOne = new Negate(new Constant(1));
+        assertThat(negOne.evaluate()).isEqualTo(-1.);
 
-    var sumTwoFour = new Addition(two, four);
-    assertThat(sumTwoFour.evaluate()).isEqualTo(6.);
+        var sumTwoFour = new Addition(two, four);
+        assertThat(sumTwoFour.evaluate()).isEqualTo(6.);
 
-    var mult = new Multiplication(sumTwoFour, negOne);
-    assertThat(mult.evaluate()).isEqualTo(-6.);
+        var mult = new Multiplication(sumTwoFour, negOne);
+        assertThat(mult.evaluate()).isEqualTo(-6.);
 
-    var exp = new Exponent(mult, 2);
-    assertThat(exp.evaluate()).isEqualTo(36.);
+        var exp = new Exponent(mult, 2);
+        assertThat(exp.evaluate()).isEqualTo(36.);
 
-    var res = new Addition(exp, new Constant(1));
-    assertThat(res.evaluate()).isEqualTo(37.);
-  }
+        var res = new Addition(exp, new Constant(1));
+        assertThat(res.evaluate()).isEqualTo(37.);
+    }
 }
--- src/test/java/edu/hw2/Task4Test.java
+++ src/test/java/edu/hw2/Task4Test.java
@@ -1,17 +1,17 @@
 package edu.hw2;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import edu.hw2.task4.CallingInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class Task4Test {
-  @Test
-  @DisplayName("Удалённый сервер")
-  void test() {
-    CallingInfo callingInfo = CallingInfo.callingInfo();
-    assertThat(callingInfo.className()).isEqualTo(Task4Test.class.getName());
-    assertThat(callingInfo.methodName()).isEqualTo("test");
-  }
+    @Test
+    @DisplayName("Удалённый сервер")
+    void test() {
+        CallingInfo callingInfo = CallingInfo.callingInfo();
+        assertThat(callingInfo.className()).isEqualTo(Task4Test.class.getName());
+        assertThat(callingInfo.methodName()).isEqualTo("test");
+    }
 }
