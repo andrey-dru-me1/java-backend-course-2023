da14fc038ff804e03f9dc732c42be3874a3f6343
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-10-29 22:42:22 +0700

    Implement 8th task

---
+++ src/test/java/edu/hw3/Task8Test.java
@@ -0,0 +1,19 @@
+package edu.hw3;
+
+import edu.hw3.task8.BackwardIterator;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class Task8Test {
+
+    @Test
+    void testTreeMap() {
+        Iterator<Integer> iterator = new BackwardIterator<>(List.of(1, 2, 3));
+        assertThat(iterator.next()).isEqualTo(3);
+        assertThat(iterator.next()).isEqualTo(2);
+        assertThat(iterator.next()).isEqualTo(1);
+    }
+}
