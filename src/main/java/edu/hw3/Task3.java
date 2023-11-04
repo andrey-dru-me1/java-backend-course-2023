08352b6065dadd0076b54dcd0422dcca9f0a0b41
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-11-05 01:46:58 +0700

    Rename res to result

--- src/main/java/edu/hw3/Task3.java
+++ src/main/java/edu/hw3/Task3.java
@@ -8,11 +8,11 @@ public class Task3 {
     }
 
     public static <T> Map<T, Integer> freqDict(T[] array) {
-        Map<T, Integer> res = new HashMap<>();
+        Map<T, Integer> result = new HashMap<>();
         for (T o : array) {
-            res.putIfAbsent(o, 0);
-            res.put(o, res.get(o) + 1);
+            result.putIfAbsent(o, 0);
+            result.put(o, result.get(o) + 1);
         }
-        return res;
+        return result;
     }
 }
