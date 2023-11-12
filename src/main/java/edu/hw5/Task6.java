7a12231d2a92f1825a444ed0e49c4882dad8e93f
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-11-13 01:18:06 +0700

    Implement 6th task

---
+++ src/main/java/edu/hw5/Task6.java
@@ -0,0 +1,12 @@
+package edu.hw5;
+
+import java.util.regex.Pattern;
+
+public class Task6 {
+
+    public static boolean isSubstring(String string, String substring) {
+        Pattern pattern = Pattern.compile(substring);
+        return pattern.matcher(string).find();
+    }
+
+}
