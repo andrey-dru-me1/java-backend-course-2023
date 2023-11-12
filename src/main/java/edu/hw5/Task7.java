e8035c3b5cc1197133b2152d60ab7eba05a15785
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-11-13 01:29:13 +0700

    Implement 7th task

---
+++ src/main/java/edu/hw5/Task7.java
@@ -0,0 +1,22 @@
+package edu.hw5;
+
+import java.util.regex.Pattern;
+
+public class Task7 {
+
+    public static boolean isThirdZero(String string) {
+        Pattern pattern = Pattern.compile("^[01]{2}0[01]*$");
+        return pattern.matcher(string).matches();
+    }
+
+    public static boolean sameStartEnd(String string) {
+        Pattern pattern = Pattern.compile("^(0[01]*0)|(1[01]*1)|$");
+        return pattern.matcher(string).matches();
+    }
+
+    public static boolean betweenOneAndThree(String string) {
+        Pattern pattern = Pattern.compile("^[01]{1,3}$");
+        return pattern.matcher(string).matches();
+    }
+
+}
