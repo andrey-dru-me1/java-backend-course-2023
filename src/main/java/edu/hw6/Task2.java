f77e54a1a42e1f7f2bed88ed5a33b7e1c2d02d15
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-11-18 22:55:57 +0700

    Implement 2nd task

---
+++ src/main/java/edu/hw6/Task2.java
@@ -0,0 +1,36 @@
+package edu.hw6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class Task2 {
+
+    private static final String COPY_STRING = " — копия";
+
+    public static void cloneFile(Path filePath) throws IOException {
+        if (!filePath.toFile().exists()) {
+            throw new IllegalArgumentException("File does not exist");
+        }
+        String fileName = filePath.getFileName().toString();
+        int lastPeriodIndex = fileName.lastIndexOf(".");
+        if (lastPeriodIndex == -1) {
+            lastPeriodIndex = fileName.length();
+        }
+        String copyFileName =
+                fileName.substring(0, lastPeriodIndex) + COPY_STRING + fileName.substring(lastPeriodIndex);
+        Path copyFilePath = filePath.resolveSibling(copyFileName);
+
+        lastPeriodIndex += COPY_STRING.length();
+        for (int i = 2; copyFilePath.toFile().exists(); i++) {
+            String indexedCopyFileName =
+                    copyFileName.substring(0, lastPeriodIndex) + " (" + i + ")" + copyFileName.substring(
+                            lastPeriodIndex);
+            copyFilePath = filePath.resolveSibling(indexedCopyFileName);
+        }
+
+        Files.copy(filePath, copyFilePath, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+}
