dae102e76d56f3d76f91c9356deb2e550615a917
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-11-19 03:47:18 +0700

    Implement 4th task

---
+++ src/main/java/edu/hw6/Task4.java
@@ -0,0 +1,29 @@
+package edu.hw6;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.CRC32C;
+import java.util.zip.CheckedOutputStream;
+
+public class Task4 {
+
+    private final static String MESSAGE = "Programming is learned by writing programs. ― Brian Kernighan";
+
+    public static void writeToFile(Path path) throws IOException {
+        try (OutputStream outputStream = Files.newOutputStream(
+                path); CheckedOutputStream checkedOutputStream = new CheckedOutputStream(outputStream,
+                new CRC32C()); BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+                checkedOutputStream); OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                bufferedOutputStream); PrintWriter printWriter = new PrintWriter(outputStreamWriter)) {
+
+            printWriter.println(MESSAGE);
+        }
+    }
+
+}
